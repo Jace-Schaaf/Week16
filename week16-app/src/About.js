593ee832fe5230516ac529f51224a2fb76f56ab8
Jace Schaaf <103080669+Jace-Schaaf@users.noreply.github.com>
2022-07-23 21:09:16 -0600

    week16 project

---
+++ week16-app/src/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <main className='About'>
+            <h2>About</h2>
+            <p style={{ marginTop: "1rem" }}>Project</p>
+        </main>
+    )
+}
+
+export default About
